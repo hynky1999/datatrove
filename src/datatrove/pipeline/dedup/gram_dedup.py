c4b6516f22f6e208de4165b712b80463916a9918
Hynek Kydlíček <kydlicek.hynek@gmail.com>
2024-05-20 04:02:48 +0200

    refactor

--- src/datatrove/pipeline/dedup/gram_dedup.py
+++ src/datatrove/pipeline/dedup/gram_dedup.py
@@ -1,5 +1,7 @@
+import bisect
+from dataclasses import dataclass, field
 import math
-from typing import Any, TypeVar
+from typing import Any, Literal, TypeVar
 from loguru import logger
 
 from datatrove.data import DocumentsPipeline
@@ -9,91 +11,146 @@ import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
 from datatrove.utils.text import TextNormConfig, simplify_text
 
+CONDITION_MODES = Literal["&", "|"]
+
+@dataclass(frozen=True)
+class KgramDeduplicationConfig:
+    """
+    Configuration for in-document kgram deduplication
+    deduplicate_over_lines: Whether to deduplicate over lines or tokens
+    deduplicated_md_tables: Whether to deduplicate markdown tables
+    text_norm_config: Text normalization configuration, before kgram deduplication
+    min_rep_size: Minimum size (chars) ofk-gram duplicates to consider for removal
+    min_reps: Minimum number of consecutive k-gram duplicates to consider for removal
+    min_k_gram: Min k-gram for removal
+    max_k_gram: Max k-gram for removal
+    condition_mode: Condition mode when evaluating min_rep_size/min_reps
+    chunk_size: Sizes of chunks to conider, to chunk the deduplicated lines/tokens into before performing the deduplication.
+        This is to avoid memory overflow.
+    """
+    deduplicate_over_lines: bool = True
+    deduplicated_md_tables: bool = True
+    text_norm_config: TextNormConfig = field(default_factory=lambda: TextNormConfig(norm_numbers=False, remove_punctuation=False, norm_whitespace=False, norm_unicode_diacritics=False, lowercase=True))
+    min_rep_size: int = 70
+    min_reps: int = 25
+    min_k_gram: int = 1
+    max_k_gram: int = 10
+    condition_mode: CONDITION_MODES = "|"
+    chunk_size: int = 100000
+
+DEFAULT_GRAM_DEDUP_CONFIG = KgramDeduplicationConfig()
+
+@dataclass(frozen=True)
+class LineDeduplicationConfig:
+    """
+    Configuration for in-document kgram deduplication
+    text_norm_config: Text normalization configuration, before kgram deduplication
+    min_rep_size: Minimum size (chars) ofk-gram duplicates to consider for removal
+    min_reps: Minimum number of consecutive k-gram duplicates to consider for removal
+    min_k_gram: Min k-gram for removal
+    max_k_gram: Max k-gram for removal
+    condition_mode: Condition mode when evaluating min_rep_size/min_reps
+    chunk_size: Sizes of chunks to conider, to chunk the deduplicated lines/tokens into before performing the deduplication.
+        This is to avoid memory overflow.
+    """
+    text_norm_config: TextNormConfig = field(default_factory=lambda: TextNormConfig(norm_numbers=False, remove_punctuation=False, norm_whitespace=False, norm_unicode_diacritics=False, lowercase=True))
+    min_rep_size: int = 1
+    min_reps: int = 1
+    min_k_gram: int = 1
+    max_k_gram: int = 3
+    condition_mode: CONDITION_MODES = "&"
+    chunk_size: int = 20000
+
+DEFAULT_LINE_DEDUP_CONFIG = LineDeduplicationConfig()
+
+@dataclass
+class Span:
+    """
+    Span of a elements to remove
+    start: Start of the span (inclusive)
+    end: End of the span (exclusive)
+    k: k-gram size
+    """
+    start: int
+    end: int
+    k: int
+
+
 
 REMOVE_SPANS_ELEMENT = TypeVar('REMOVE_SPANS_ELEMENT')
-def remove_spans(elements: list[REMOVE_SPANS_ELEMENT], spans: list[tuple[int, int]]) -> list[REMOVE_SPANS_ELEMENT]:
+def remove_spans(elements: list[REMOVE_SPANS_ELEMENT], spans: list[Span]) -> list[REMOVE_SPANS_ELEMENT]:
     """
-    Remove spans from elements
+    Remove spans of elements
     Args:
         elements: List of elements
-        spans: List of [start, end) tuples
+        spans: List of spans (Expected to be non-overlapping, otherwise the result is undefined)
+    Returns:
+        List of elements without the spans
     """
     elements_to_keep = []
-    spans = sorted(spans, key=lambda x: (x[0], -x[1]))
+    spans = sorted(spans, key=lambda x: (x.start, -x.end))
     cur_idx = 0
-    for start, end in spans:
-        start = max(cur_idx, start)
+    for span in spans:
+        start = max(cur_idx, span.start)
         elements_to_keep.extend(elements[cur_idx:start])
-        cur_idx = end
+        cur_idx = span.end
     elements_to_keep.extend(elements[cur_idx:])
     return elements_to_keep
     
-def select_non_overlapping_spans(spans_to_remove_list: list[tuple[int, int, int]]):
+def select_non_overlapping_spans(spans_to_remove_list: list[Span]) -> list[Span]:
     """
-    Selects spans which don't overlap, while tyring to maxime coverage of the spans
+    Selects spans which don't overlap, while tyring to maxime coverage of the spans.
+        If the span doesn't fit, it tries to shorten the span to fit (always by span.k).
     Args:
-        spans_to_remove_list: List of spans to remove
+        spans_to_remove_list: List of spans, where span.start <= span.end
+    
+    Returns:
+        List of non-overlapping spans, sorted by span.start
     """
     if len(spans_to_remove_list) == 0:
         return []
 
     # Sort the spans, first by span_size to cover with the biggest reps first, then by start
     # -> For same k start_i-1 <= start_i
-    spans_to_remove_list = sorted(spans_to_remove_list, key=lambda x: (-(x[1]-x[0]), x[0], -x[1]))
-    last_start = spans_to_remove_list[0][2]
-    cur_idx = 0
-    # Rule: ordered by span[0] + no overlap
+    spans_to_remove_list = sorted(spans_to_remove_list, key=lambda x: (-(x.end-x.start), x.start, -x.end))
     # Use dummy to ensure that there is always a span to the left
-    new_spans = [(-1,-1,1)]
-    # Rule: new_spans[cur_idx][0] <= span[0]
-    # Thus assuming for all span[1] > span[0] -> new_spans[cur_idx][1] > span[0]
-    # The second assumption only doesn't hold for the dummy span at the start
+    selected_spans: list[Span] = [Span(-1,-1,1)]
     for span in spans_to_remove_list:
-        new_span_start = span[0]
-        new_span_end = span[1]
-        # We expect that last_start >= span[0], otherwise we have to reset
-        if span[0] < last_start:
-            cur_idx = 0
+        new_span_end, new_span_start = span.end, span.start
 
-        left_idx = cur_idx
-        # Search for first left span, that is the last one for which s_start < span[0]
-        while left_idx + 1 < len(new_spans) and new_spans[left_idx+1][0] < span[0]:
-            left_idx += 1
+        # Index of the span which is the last one for which selected_spans[left_idx].start < span.start (closest to the left)
+        left_idx = bisect.bisect_left(selected_spans, span.start, key=lambda x: x.start) - 1
         
         # If there is an overlap with the left span
-        if left_idx != len(new_spans) and new_spans[left_idx][1] > span[0]:
+        if left_idx != len(selected_spans) and selected_spans[left_idx].end > span.start:
             # Shorten left border to remove overlap
-            shorten_times = math.ceil((new_spans[left_idx][1] - span[0]) / span[2])
-            new_span_start = span[0] + span[2] * shorten_times
+            shorten_times = math.ceil((selected_spans[left_idx].end - span.start) / span.k)
+            new_span_start = span.start + span.k * shorten_times
 
-        # Then search for the first right span that is the first one for which s_end > span[1]
-        right_idx = cur_idx
-        while right_idx < len(new_spans) and new_spans[right_idx][1] <= span[1]:
-            right_idx += 1
+        # Index of the span which is the first one for which selected_spans[left_idx].end < span.end
+        right_idx = bisect.bisect_right(selected_spans, span.end, key=lambda x: x.end, lo=left_idx)
         
         # If there is an overlap with the right span
-        if right_idx != len(new_spans) and new_spans[right_idx][0] < span[1]:
+        if right_idx != len(selected_spans) and selected_spans[right_idx].start < span.end:
             # Shorten the right border to remove overlap
-            shorten_times = math.ceil((span[1] - new_spans[right_idx][0]) / span[2])
-            new_span_end = span[1] - span[2] * (shorten_times)
+            shorten_times = math.ceil((span.end - selected_spans[right_idx].start) / span.k)
+            new_span_end = span.end - span.k * (shorten_times)
 
-        # If the span is no longer valid don't add it and move the cur_idx
-        if new_span_end - new_span_start < span[2] * 2:
-            cur_idx = left_idx
+        # If the span is no longer valid don't add it
+        if new_span_end - new_span_start < span.k * 2:
             continue
 
-        new_spans = new_spans[:left_idx+1] + [(new_span_start, new_span_end, span[2])] + new_spans[right_idx:]
-        cur_idx = left_idx+1
+        selected_spans = selected_spans[:left_idx+1] + [Span(new_span_start, new_span_end, span.k)] + selected_spans[right_idx:]
             
     # Check that the spans are non-overlapping
-    assert all(new_spans[i][0] < new_spans[i+1][0] for i in range(len(new_spans) - 1))
+    assert all(selected_spans[i].start < selected_spans[i+1].start for i in range(len(selected_spans) - 1))
     # Remove the dummy span
-    return new_spans[1:]
+    return selected_spans[1:]
 
 
 def get_dup_consequtive_kgram_ids(sequence: np.ndarray, tokens_cum_sum: np.ndarray, k: int, min_rep: int, min_size: int, condition_mode: str):
     """
-    Takes a sequence and 2xI matrix of spans signifying the start and end of the span which are duplicated
+    Takes a sequence and returns 2xI matrix of spans signifying the start and end of the span which are duplicated
     """
     if len(sequence) // 2 < k:
         return np.array([])
@@ -147,7 +204,6 @@ def get_dup_consequtive_mutli_kgram_ids(tokens: list[str], min_k: int, max_k: in
     tokens = ["a", "b", "a", "b", "d"]
     -> [0, 1]
 
-
     Args:
         tokens: List of tokens
         min_k: Minimum k-gram size
@@ -160,7 +216,7 @@ def get_dup_consequtive_mutli_kgram_ids(tokens: list[str], min_k: int, max_k: in
 
 
     # First convert string tokens to integers, for multiple k this allows faster comparisson
-    spans_to_remove = []
+    spans_to_remove: list[Span] = []
     for start_split in range(0, len(tokens), max_split_size - split_overlap):
         end_split = start_split + max_split_size
         split_tokens = tokens[start_split:end_split]
@@ -169,41 +225,33 @@ def get_dup_consequtive_mutli_kgram_ids(tokens: list[str], min_k: int, max_k: in
         tokens_cum_sum = np.cumsum([len(token) for token in split_tokens])
         for k in range(min_k, max_k + 1):
             new_spans_to_remove = get_dup_consequtive_kgram_ids(int_tokens, tokens_cum_sum, k, min_rep, min_size, condition_mode)
-            offseted_idx_to_remove = [(start + start_split, end + start_split, k) for start, end in new_spans_to_remove]
-            spans_to_remove.extend(offseted_idx_to_remove)
+            new_span_with_k = [Span(start + start_split, end + start_split, k) for start, end in new_spans_to_remove]
+            spans_to_remove.extend(new_span_with_k)
     
     filtered_spans_to_remove = select_non_overlapping_spans(spans_to_remove)
-    with_removed_last = [(start, end-k) for start, end, k in filtered_spans_to_remove]
+    with_removed_last = [Span(span.start, span.end-span.k, span.k) for span in filtered_spans_to_remove]
     return with_removed_last
 
 
 class GramDeduplication(PipelineStep):
-    def __init__(self, text_norm_config: TextNormConfig = TextNormConfig(norm_numbers=False, remove_punctuation=False), deduplicated_md_tables: bool = False, deduplicate_over_lines: bool = False, min_span_size: int=45, min_k_gram: int = 1, max_k_gram: int = 10, min_rep: int = 1, condition_mode: str = "|", span_size: int=20000):
+    def __init__(self, dedup_config: KgramDeduplicationConfig = DEFAULT_GRAM_DEDUP_CONFIG):
         from nltk import NLTKWordTokenizer
         tokenizer = NLTKWordTokenizer()
-        self.min_span_size = min_span_size
-        self.min_k_gram = min_k_gram
-        self.max_k_gram = max_k_gram
-        self.min_rep = min_rep
-        self.span_size = span_size
-        self.deduplicated_md_tables = deduplicated_md_tables
-        self.deduplicate_over_lines = deduplicate_over_lines
-        self.condition_mode = condition_mode
-        self.text_norm_config = text_norm_config
         self.tokenizer = tokenizer
+        self.dedup_config = dedup_config
     
     def dedup_text(self, text: str):
         # Heuristic for not deduplicating tables
-        if '|' in text and not self.deduplicated_md_tables:
+        if '|' in text and not self.dedup_config.deduplicated_md_tables:
             return text
 
         spans = list(self.tokenizer.span_tokenize(text))
-        tokens = [simplify_text(text[start:end], self.text_norm_config) for start, end in spans]
+        tokens = [simplify_text(text[start:end], self.dedup_config.text_norm_config) for start, end in spans]
         # Since we are always removing preceeding repetitions, we want to also remove their whitespaces
         # Thus we use right delimiter
         tokens_with_right_delimiter = [text[span_prev[0]:span_next[0]] for span_prev, span_next in zip(spans[:-1], spans[1:])] + [text[spans[-1][0]:]]
-        overlap = self.max_k_gram * (self.min_rep + 1)
-        spans_to_remove = get_dup_consequtive_mutli_kgram_ids(tokens, self.min_k_gram, self.max_k_gram, self.min_rep, self.min_span_size, self.condition_mode, self.span_size, overlap)
+        overlap = self.dedup_config.max_k_gram * (self.dedup_config.min_reps + 1)
+        spans_to_remove = get_dup_consequtive_mutli_kgram_ids(tokens, self.dedup_config.min_k_gram, self.dedup_config.max_k_gram, self.dedup_config.min_reps, self.dedup_config.min_rep_size, self.dedup_config.condition_mode, self.dedup_config.chunk_size, overlap)
         if len(spans_to_remove) == 0:
             return text
         
@@ -215,7 +263,7 @@ class GramDeduplication(PipelineStep):
     def run(self, data: DocumentsPipeline, rank: int = 0, world_size: int = 1) -> DocumentsPipeline:
         for document in data:
             # Prefer line by line 
-            lines = document.text.split("\n") if not self.deduplicate_over_lines else [document.text]
+            lines = document.text.split("\n") if not self.dedup_config.deduplicate_over_lines else [document.text]
             deduplicated_lines = [self.dedup_text(line) for line in lines]
             document.text = "\n".join(deduplicated_lines)
         return data
@@ -223,22 +271,15 @@ class GramDeduplication(PipelineStep):
 
 
 class LineDeduplication(PipelineStep):
-    def __init__(self, text_norm_config: TextNormConfig = TextNormConfig(norm_numbers=False, remove_punctuation=False), min_span_size: int=1, min_k_gram: int = 1, max_k_gram: int = 3, min_rep: int = 1, condition_mode="&" ,span_size: int=20000):
-        if min_span_size < 1:
-            logger.warning("It's recommended to have min_span_size >= 1 so that consecutive newlines are not considered duplicates")
+    def __init__(self, dedup_config: LineDeduplicationConfig = DEFAULT_LINE_DEDUP_CONFIG):
+        self.dedup_config = dedup_config
+        if self.dedup_config.min_rep_size < 1:
+            logger.warning("It's recommended to have min_rep_size >= 1 so that consecutive newlines are not considered duplicates")
 
         super().__init__()
 
         from nltk import NLTKWordTokenizer
         tokenizer = NLTKWordTokenizer()
-        self.min_span_size = min_span_size
-        self.min_k_gram = min_k_gram
-        self.max_k_gram = max_k_gram
-        self.min_rep = min_rep
-        self.text_norm_config = text_norm_config
-        self.span_size = span_size
-        self.condition_mode = condition_mode
-
         self.tokenizer = tokenizer
     
 
@@ -246,11 +287,11 @@ class LineDeduplication(PipelineStep):
         for document in data:
             # Prefer line by line 
             lines = document.text.split("\n")
-            overlap = self.max_k_gram * (self.min_rep + 1)
-            simplified_lines = [simplify_text(line, self.text_norm_config) for line in lines]
+            overlap = self.dedup_config.max_k_gram * (self.dedup_config.min_reps + 1)
+            simplified_lines = [simplify_text(line, self.dedup_config.text_norm_config) for line in lines]
             span_to_remove = get_dup_consequtive_mutli_kgram_ids(
                 simplified_lines,
-                self.min_k_gram, self.max_k_gram, self.min_rep, self.min_span_size, self.condition_mode, self.span_size, overlap)
+                self.dedup_config.min_k_gram, self.dedup_config.max_k_gram, self.dedup_config.min_reps, self.dedup_config.min_rep_size, self.dedup_config.condition_mode, self.dedup_config.chunk_size, overlap)
             if len(span_to_remove) > 0:
                 # Use non-simplified lines
                 document.text = "\n".join(remove_spans(lines, span_to_remove))
--- tests/pipeline/test_repetition_deduplication.py
+++ tests/pipeline/test_repetition_deduplication.py
@@ -1,39 +1,38 @@
 import unittest
-from datatrove.pipeline.dedup.gram_dedup import GramDeduplication, LineDeduplication, select_non_overlapping_spans
+from datatrove.pipeline.dedup.gram_dedup import DEFAULT_GRAM_DEDUP_CONFIG, DEFAULT_LINE_DEDUP_CONFIG, GramDeduplication, LineDeduplication, Span, select_non_overlapping_spans
 from datatrove.pipeline.base import Document
-from datatrove.utils.text import TextNormConfig
+from dataclasses import replace
 
-def span_overlapping(spans: list[tuple[int, int, int]]) -> bool:
+def span_overlapping(spans: list[Span]) -> bool:
     last_end = 0
     for span in spans:
-        if span[0] < last_end:
+        if span.start < last_end:
             return True
-        last_end = span[1]
+        last_end = span.end
     return False
 
-def span_coverage(spans: list[tuple[int, int, int]]) -> int:
-    return sum([span[1] - span[0] for span in spans])
+def span_coverage(spans: list[Span]) -> int:
+    return sum([span.end - span.start for span in spans])
 
 
 class TestRepetitionDeduplication(unittest.TestCase):
     def test_gram_deduplication(self):
         docs = [Document("e? Can you? Can you?” Well", "0")]
-        gram_dedup = GramDeduplication(min_span_size = 0)
+        gram_dedup = GramDeduplication(dedup_config=replace(DEFAULT_GRAM_DEDUP_CONFIG, min_rep_size=0))
         dedup_docs = gram_dedup.run(docs)
         expected = "e? Can you?” Well"
         self.assertEqual(dedup_docs[0].text, expected)
     
     def test_line_deduplication(self):
         docs = [Document("A\n\nB\n\nB\nC\n\nD", "0")]
-        line_dedup = LineDeduplication()
+        line_dedup = LineDeduplication(dedup_config=replace(DEFAULT_LINE_DEDUP_CONFIG, min_rep_size=0))
         dedup_docs = line_dedup.run(docs)
         expected = "A\n\nB\nC\n\nD"
         self.assertEqual(dedup_docs[0].text, expected)
     
-
     def test_span_selection(self):
         spans_to_remove_list = [
-            (0, 4, 2), (4, 6, 1), (0, 1, 1),
+            Span(0, 4, 2), Span(4, 6, 1), Span(0, 1, 1),
         ]
 
         selected_spans = select_non_overlapping_spans(spans_to_remove_list)
@@ -42,7 +41,7 @@ class TestRepetitionDeduplication(unittest.TestCase):
 
     def test_span_selection_non_overlapping_1(self):
         spans_to_remove_list = [
-            (1, 5, 2), (5, 7, 1), (0, 10, 1)
+            Span(1, 5, 2), Span(5, 7, 1), Span(0, 10, 1)
         ]
 
         selected_spans = select_non_overlapping_spans(spans_to_remove_list)
@@ -51,7 +50,7 @@ class TestRepetitionDeduplication(unittest.TestCase):
 
     def test_span_selection_non_overlapping_2(self):
         spans_to_remove_list = [
-            (1, 5, 2), (20, 22, 1)
+            Span(1, 5, 2), Span(20, 22, 1)
         ]
 
         selected_spans = select_non_overlapping_spans(spans_to_remove_list)
